274517c2d40af776fb1f98608a07875f9c2b0ab3
wastedx7 <wastedx7@gmail.com>
2026-02-10 18:31:27 +0530

    project initialised

---
+++ java/smartattendance/src/test/java/com/smartattendance/SmartattendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartattendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartattendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
